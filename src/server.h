22c570dc594e14eaf1f151917389c703efeb3ab2
Daniel Neugebauer <dneuge@energiequant.de>
2023-04-02 21:36:33 +0200

    reference dataproxy_registry_t, stub if not building the plugin

--- src/server.h
+++ src/server.h
@@ -11,12 +11,21 @@ typedef struct _server_t server_t;
 #include "network.h"
 #include "task_schedule.h"
 
+#ifdef BUILD_PLUGIN
+#include "dataproxy.h"
+#else
+// in case we are not building the plugin (but test code/utils)
+// we need to stub unused type definitions instead of importing
+// plugin code to break compile-time dependency on those files
+typedef struct {} dataproxy_registry_t;
+#endif
+
 typedef struct {
     char *password;
     network_server_config_t network;
     task_schedule_t *task_schedule;
     command_factory_t *command_factory;
-    void *dataproxy_registry; // importing dataproxy.h requires X-Plane SDK which breaks test compilation
+    dataproxy_registry_t *dataproxy_registry;
 } server_config_t;
 
 typedef struct _server_t {
