0bfb24073a776631e97bbcb9bb000a7473e65669
Daniel Neugebauer <dneuge@energiequant.de>
2023-04-11 13:41:11 +0200

    emacs: fix false positives about type redefinition

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,6 +5,7 @@
     
     ; TODO: set up for and switch to GCC if somehow possible... plugin is expected to be compiled with GCC, not clang
     (flycheck-clang-definitions . ("APL=0" "IBM=0" "LIN=1"
+                                   "BUILD_PLUGIN=1"
                                    "XPLM200=1" "XPLM210=1" "XPLM300=1" "XPLM301=1" "XPLM303=1" "XPLM400=1"
                                    ))
     (flycheck-clang-include-path . ("../lib/XPSDK/CHeaders/Widgets" "../lib/XPSDK/CHeaders/Wrappers" "../lib/XPSDK/CHeaders/XPLM"))
