7953a9f7841987ea287664f6a16577003c140195
Daniel Neugebauer <dneuge@energiequant.de>
2023-08-13 19:10:17 +0200

    renamed test-password-generator.c as it is a manual test

--- src/manualtest-password-generator.c
+++ src/manualtest-password-generator.c
@@ -3,6 +3,11 @@
 
 #include "password_generator.h"
 
+/* Repeatedly calls generate_password() and prints the resulting strings for manual testing.
+ *
+ * The wanted number of iterations can be provided as a command-line argument.
+ */
+
 int main(int argc, char **argv) {
     int num_iterations = 10;
     if (argc > 1) {
