9d04b3b88c59cd2e464f768f9aa69723e23e48f7
Daniel Neugebauer <dneuge@energiequant.de>
2023-09-10 18:42:53 +0200

    added manualtest-network-interfaces

---
+++ src/manualtest-network-interfaces.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "network.h"
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("call with 1 to include IPv6 addresses, 0 to only show IPv4\n");
+        return 1;
+    }
+    bool include_ipv6 = (atoi(argv[1]) == 1);
+
+    list_t *interfaces = get_network_interfaces(include_ipv6);
+    if (!interfaces) {
+        printf("no additional interfaces found\n");
+        return 1;
+    }
+
+    list_item_t *item = interfaces->head;
+    int i = 0;
+    while (item) {
+        printf("%2d %s\n", i++, (char*) item->value);
+        item = item->next;
+    }
+
+    destroy_list(interfaces, free);
+
+    return 0;
+}
