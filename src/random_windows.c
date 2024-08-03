fe5c3453de1493cd417e2c66b6bf2de0e43b0819
Daniel Neugebauer <dneuge@energiequant.de>
2024-08-03 18:04:12 +0200

    Windows compatibility: insecure random number generation

--- src/random_windows.c
+++ src/random_windows.c
@@ -1,4 +1,5 @@
-#include <stddef.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "random.h"
 
@@ -8,10 +9,19 @@ int* get_random_ints(int count, int min, int max) {
 }
 
 long get_random_long_insecure() {
-    // FIXME: implement for Windows
-    return 42;
+    long res = 0;
+
+    // NOTE: sizeof(int) is unreliable in the way that it over-reports the actual length on x86 to the size of a long,
+    //       probably because it is handled as a long internally. Instead of "concatenating" ints we take the lowest
+    //       byte of a random and construct a long byte-by-byte, just to make sure this has really the intended effect
+    //       regardless of compiler/architecture specifics.
+    for (int i=0; i<sizeof(long); i++) {
+        res = (res << 8) ^ (rand() & 0xFF);
+    }
+
+    return res;
 }
 
 void initialize_insecure_random() {
-    // FIXME: implement for Windows
+    srand(time(NULL));
 }
