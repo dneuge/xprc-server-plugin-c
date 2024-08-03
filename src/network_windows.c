0070cc49be62d0d73cf861d794dabae8fceb9a8c
Daniel Neugebauer <dneuge@energiequant.de>
2024-08-03 14:25:09 +0200

    Windows compatibility: get_network_interfaces

--- src/network_windows.c
+++ src/network_windows.c
@@ -1,6 +1,38 @@
+/* The goal of this file is to establish compatibility with Windows operating systems.
+ *
+ * This file is based on API information published by Microsoft under CC-BY 4.0 and MIT licenses at:
+ *
+ *   https://github.com/MicrosoftDocs/sdk-api
+ *   revision 5da3012685fee3b1dbbefe7fa1f9a9935b9fa14e (2 Aug 2024)
+ *   see repository at specified revision for detailed license information
+ *
+ * Official API documentation omits headers and thus low-level type information. Missing information has
+ * been substituted in reference to headers distributed as part of wine which are published under terms of
+ * LGPL 2.1:
+ *
+ *   https://github.com/wine-mirror/wine/blob/master/include/
+ *
+ * This file itself remains published under MIT license. If one of the API reference sources requires a more
+ * restrictive license to be put into effect, the respective license shall take precedence with closely limited effect
+ * in accordance to the right to sublicense MIT-licensed works. This will mainly affect binary distributions and
+ * other code that might be based upon this file. To avoid licensing issues and to avoid taking over a potentially
+ * wrong implementation, it is strongly recommended not to use this file for reference in other projects; follow
+ * the original API docs on your own instead.
+ */
+
 #include <stddef.h>
+#include <stdlib.h>
+
+// Windows APIs
+#include <winsock2.h>
+#include <iphlpapi.h>
 
 #include "network.h"
+
+#include "utils.h"
+
+#define MAX_ADDRESS_RESULT_BUFFER_SIZE (1 * 1024 * 1024) /* 1MB should be enough for everyone */
+#define ADDRESS_STRING_BUFFER_SIZE 40 /* IPv6: 8x 4 hex chars + 7 dividers + 1 NULL terminator */
 
 error_t create_network_server(network_server_t **server, network_server_config_t *config, network_handler_t handler) {
     // FIXME: implement for Windows
@@ -22,6 +54,128 @@ void close_network_connection(network_connection_t *connection) {
 }
 
 list_t* get_network_interfaces(bool include_ipv6) {
-    // FIXME: implement for Windows
+    // Microsoft API docs:
+    //   docs/sdk-api-src/content/iphlpapi/nf-iphlpapi-getadaptersaddresses.md
+    //   docs/sdk-api-src/content/iptypes/ns-iptypes-ip_adapter_addresses_lh.md
+    //   docs/sdk-api-src/content/winsock2/nf-winsock2-wsaaddresstostringa.md
+
+    void *result_buffer = NULL;
+    list_t *out = NULL;
+
+    // GetAdaptersAddresses needs a fully pre-allocated result buffer. Microsoft recommends to start with 15kB for a
+    // first attempt. Due to the high number of network interfaces usually encountered these days we start with 64kB
+    // instead.
+    long unsigned int result_buffer_size = 64 * 1024;
+    while (true) {
+        if (result_buffer) {
+            free(result_buffer);
+            result_buffer = NULL;
+        }
+
+        long unsigned int original_result_buffer_size = result_buffer_size;
+        result_buffer = zalloc(result_buffer_size);
+        if (!result_buffer) {
+            goto error;
+        }
+
+        unsigned long res = GetAdaptersAddresses(
+                /* Family           */ include_ipv6 ? AF_UNSPEC : AF_INET,
+                /* Flags            */GAA_FLAG_SKIP_MULTICAST | GAA_FLAG_SKIP_DNS_SERVER | GAA_FLAG_SKIP_FRIENDLY_NAME |
+                                      GAA_FLAG_INCLUDE_ALL_INTERFACES,
+                /* (Reserved)       */ NULL,
+                /* AdapterAddresses */ result_buffer,
+                /* SizePointer      */ &result_buffer_size
+        );
+
+        // exit loop if successful
+        if (res == ERROR_SUCCESS) {
+            break;
+        } else if (res == ERROR_BUFFER_OVERFLOW) {
+            // Windows indicates that our buffer is too small and has modified result_buffer_size to indicate
+            // the suggested size for the next attempt.
+            // We only retry if that requested size is actually larger than before and while it is within limit.
+            if ((result_buffer_size <= original_result_buffer_size) ||
+                (result_buffer_size > MAX_ADDRESS_RESULT_BUFFER_SIZE)) {
+                goto error;
+            }
+
+            continue;
+        }
+
+        // something went wrong; abort
+        goto error;
+    }
+
+    out = create_list();
+    if (!out) {
+        goto error;
+    }
+
+    IP_ADAPTER_ADDRESSES *item = (IP_ADAPTER_ADDRESSES *) result_buffer;
+    while (item) {
+        IP_ADAPTER_UNICAST_ADDRESS_XP *unicast_item = item->FirstUnicastAddress;
+        while (unicast_item) {
+            SOCKET_ADDRESS address_container = unicast_item->Address;
+
+            // WSAAddressToString expects the length to be indicated for either regular chars or WCHAR (Unicode)
+            // but has no parameter to indicate what we actually meant to provide nor does it tell us what it
+            // wrote afterwards, duh...
+            // To be on the safe side we reserve 4 bytes for every character we want to receive which would fit UTF32.
+            // We may end up copying garbage to the output list in the end but it should gracefully fail by producing
+            // something users can't select as it's not a parseable address (probably would just show up as an empty
+            // or severely shortened string).
+            long unsigned int address_string_length = ADDRESS_STRING_BUFFER_SIZE;
+            char *address_string = zalloc(address_string_length * 4);
+            if (!address_string) {
+                goto error;
+            }
+
+            unsigned long res = WSAAddressToString(
+                    /* lpsaAddress             */ address_container.lpSockaddr,
+                    /* dwAddressLength         */ address_container.iSockaddrLength,
+                    /* lpProtocolInfo          */ NULL,
+                    /* lpszAddressString       */ address_string,
+                    /* lpdwAddressStringLength */ &address_string_length
+            );
+
+            if (res) {
+                // error
+                free(address_string);
+                address_string = NULL;
+            } else {
+                // no error
+                char *copy = copy_partial_string(address_string, address_string_length);
+                free(address_string);
+                address_string = NULL;
+
+                if (!copy) {
+                    goto error;
+                }
+
+                if (!list_append(out, copy)) {
+                    free(copy);
+                    goto error;
+                }
+            }
+
+            unicast_item = unicast_item->Next;
+        }
+
+        item = item->Next;
+    }
+
+    free(result_buffer);
+
+    return out;
+
+error:
+    if (out) {
+        destroy_list(out, free);
+    }
+
+    if (result_buffer) {
+        free(result_buffer);
+    }
+
     return NULL;
 }
