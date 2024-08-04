8cbb6b3947e9257375afc637a2b1e2e45cf0c673
Daniel Neugebauer <dneuge@energiequant.de>
2024-08-04 16:51:18 +0200

    remove unnecessary todos

--- src/network_windows.c
+++ src/network_windows.c
@@ -55,27 +55,6 @@ error_t initialize_os_network_apis() {
     return ERROR_NONE;
 }
 
-/*
-error_t create_network_server(network_server_t **server, network_server_config_t *config, network_handler_t handler) {
-    // FIXME: implement for Windows
-    return ERROR_UNSPECIFIC;
-}
-
-bool destroy_network_server(network_server_t *server) {
-    // FIXME: implement for Windows
-    return true;
-}
-
-error_t send_to_network(network_connection_t *connection, char *content, int length) {
-    // FIXME: implement for Windows
-    return ERROR_UNSPECIFIC;
-}
-
-void close_network_connection(network_connection_t *connection) {
-    // FIXME: implement for Windows
-}
-*/
-
 list_t* get_network_interfaces(bool include_ipv6) {
     // Microsoft API docs:
     //   docs/sdk-api-src/content/iphlpapi/nf-iphlpapi-getadaptersaddresses.md
